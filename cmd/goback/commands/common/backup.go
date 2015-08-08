df8ecb746d26d18da0692633032253cfc93731ab
André Bierlein <andre.mt.bierlein@gmail.com>
2015-08-08 08:11:57 +0200

    Actually use the --storage parameter

--- cmd/goback/commands/common/backup.go
+++ cmd/goback/commands/common/backup.go
@@ -1,14 +1,30 @@
 package common
 
 import (
+	"log"
+	"os"
+
 	"github.com/codegangsta/cli"
 	"github.com/twcclan/goback/backup"
 )
 
+func getStorageLocation(c *cli.Context) string {
+	loc := c.GlobalString("storage")
+
+	err := os.MkdirAll(loc, os.FileMode(0700))
+	if err != nil {
+		if os.IsExist(err) {
+			log.Fatal(err)
+		}
+	}
+
+	return loc
+}
+
 func GetChunkStore(c *cli.Context) backup.ChunkStore {
-	return backup.NewSimpleChunkStore("storage")
+	return backup.NewSimpleChunkStore(getStorageLocation(c))
 }
 
 func GetIndex(c *cli.Context) backup.Index {
-	return backup.NewSqliteIndex("storage")
+	return backup.NewSqliteIndex(getStorageLocation(c))
 }
