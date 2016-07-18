3e1050a5ddd83fd12fe120301e63007157191f55
André Bierlein <andre.mt.bierlein@gmail.com>
2016-07-18 23:14:13 +0200

    Rename callback function type for ObjectStore

--- backup/storage.go
+++ backup/storage.go
@@ -4,11 +4,11 @@ import (
 	"github.com/twcclan/goback/proto"
 )
 
-type ChunkWalkFn func(*proto.Ref) error
+type ObjectReceiver func(*proto.Ref) error
 
 type ObjectStore interface {
 	Put(*proto.Object) error
 	Get(*proto.Ref) (*proto.Object, error)
 	Delete(*proto.Ref) error
-	Walk(proto.ObjectType, ChunkWalkFn) error
+	Walk(proto.ObjectType, ObjectReceiver) error
 }
