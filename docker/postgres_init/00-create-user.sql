a364d943163610529d26f6e19961023394718373
André Bierlein <andre.mt.bierlein@gmail.com>
2020-06-28 11:04:38 +0200

    Add permission to access sequences

--- docker/postgres_init/00-create-user.sql
+++ docker/postgres_init/00-create-user.sql
@@ -2,3 +2,4 @@
 create database goback;
 create user goback with password 'goback';
 grant all privileges on database goback to goback;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO goback;
