c2dd902056e64945bffa09bf0469b1cfe073d8cf
André Bierlein <andre.mt.bierlein@gmail.com>
2021-05-11 20:40:53 +0200

    Add Counter and Collector interfaces

--- backup/storage.go
+++ backup/storage.go
@@ -3,13 +3,17 @@ package backup
 import (
 	"context"
 	"errors"
+	"io"
 
 	"github.com/twcclan/goback/proto"
 )
 
 type ObjectReceiver func(*proto.Object) error
 
-var ErrNotImplemented = errors.New("the store doesn't implement this feature")
+var (
+	ErrNotImplemented = errors.New("the store doesn't implement this feature")
+	ErrAlreadyMarked  = errors.New("object already marked")
+)
 
 //go:generate go run github.com/vektra/mockery/v2 --testonly --inpackage --name ObjectStore
 type ObjectStore interface {
@@ -18,4 +22,13 @@ type ObjectStore interface {
 	Delete(context.Context, *proto.Ref) error
 	Walk(context.Context, bool, proto.ObjectType, ObjectReceiver) error
 	Has(context.Context, *proto.Ref) (bool, error)
+}
+
+type Counter interface {
+	Count() (total uint64, unique uint64, err error)
+}
+
+type Collector interface {
+	io.Closer
+	Mark(ref *proto.Ref) error
 }
