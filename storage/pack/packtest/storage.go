f8458f03e9ca785afd3d0cbbed14552c5aa937a0
André Bierlein <andre.mt.bierlein@gmail.com>
2022-01-16 14:10:26 +0100

    Re-organize archive storage conformance tests

--- storage/pack/packtest/storage.go
+++ storage/pack/packtest/storage.go
@@ -15,21 +15,134 @@ import (
 )
 
 func TestArchiveStorage(t *testing.T, store pack.ArchiveStorage) {
+	type test struct {
+		name string
+		fn   func(t *testing.T, store pack.ArchiveStorage, files []file)
+	}
+
+	tests := []test{
+		{"create files", testCreateFiles},
+		{"read files", testReadFiles},
+		{"missing file", testMissingFiles},
+		{"list files", testListFiles},
+	}
+
+	// get test data
+	files := getStorageTestFiles(t)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			test.fn(t, store, files)
+		})
+	}
+}
+
+func testCreateFiles(t *testing.T, store pack.ArchiveStorage, files []file) {
+	// try to store all the files
+	for _, f := range files {
+		file, err := store.Create(f.key)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		n, err := file.Write(f.data)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if n != len(f.data) {
+			t.Fatal("didn't write full file content")
+		}
+
+		err = file.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func testMissingFiles(t *testing.T, store pack.ArchiveStorage, files []file) {
+	file, err := store.Open("this file probably does not exist")
+	if err != pack.ErrFileNotFound {
+		t.Fatal("expected open to return error")
+	}
+
+	if file != nil {
+		t.Fatal("expected file to be nil")
+	}
+}
+
+func testReadFiles(t *testing.T, store pack.ArchiveStorage, files []file) {
+	for _, f := range files {
+		file, err := store.Open(f.key)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		info, err := file.Stat()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff := cmp.Diff(info.Name(), path.Base(f.key)); diff != "" {
+			t.Error("file name doesn't match")
+			t.Fatal(diff)
+		}
+
+		if diff := cmp.Diff(info.Size(), int64(len(f.data))); diff != "" {
+			t.Error("file size doesn't match")
+			t.Fatal(diff)
+		}
+
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff := cmp.Diff(data, f.data); diff != "" {
+			t.Error("data doesn't match")
+			t.Fatal(diff)
+		}
+	}
+}
+
+func testListFiles(t *testing.T, store pack.ArchiveStorage, files []file) {
+	names, err := store.List()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var expected []string
+	for _, f := range files {
+		if path.Ext(f.key) == ".goback" {
+
+			name := strings.TrimSuffix(path.Base(f.key), path.Ext(f.key))
+			expected = append(expected, name)
+		}
+	}
+
+	if diff := cmp.Diff(expected, names); diff != "" {
+		t.Error("unexpected list of files")
+		t.Fatal(diff)
+	}
+}
+
+type file struct {
+	key  string
+	data []byte
+}
+
+func getStorageTestFiles(t *testing.T) []file {
+	t.Helper()
+
 	// need to get path of current file to locate the testdata folder
 	_, filename, _, ok := runtime.Caller(0)
 	if !ok {
-		t.Fatal("couldnt get caller")
+		t.Fatal("couldn't get caller")
 	}
 	t.Log(filepath.FromSlash(filename))
 
 	root := filepath.Join(filepath.Dir(filename), "testdata")
-
-	// create test data
-
-	type file struct {
-		key  string
-		data []byte
-	}
 
 	var files []file
 
@@ -66,93 +179,5 @@ func TestArchiveStorage(t *testing.T, store pack.ArchiveStorage) {
 		t.Fatal("no test files loaded")
 	}
 
-	t.Run("create files", func(t *testing.T) {
-		// try to store all the files
-		for _, f := range files {
-			file, err := store.Create(f.key)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			n, err := file.Write(f.data)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if n != len(f.data) {
-				t.Fatal("didn't write full file content")
-			}
-
-			err = file.Close()
-			if err != nil {
-				t.Fatal(err)
-			}
-		}
-	})
-
-	t.Run("read files", func(t *testing.T) {
-		for _, f := range files {
-			file, err := store.Open(f.key)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			info, err := file.Stat()
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if diff := cmp.Diff(info.Name(), path.Base(f.key)); diff != "" {
-				t.Error("file name doesn't match")
-				t.Fatal(diff)
-			}
-
-			if diff := cmp.Diff(info.Size(), int64(len(f.data))); diff != "" {
-				t.Error("file size doesn't match")
-				t.Fatal(diff)
-			}
-
-			data, err := ioutil.ReadAll(file)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if diff := cmp.Diff(data, f.data); diff != "" {
-				t.Error("data doesn't match")
-				t.Fatal(diff)
-			}
-		}
-	})
-
-	t.Run("missing file", func(t *testing.T) {
-		file, err := store.Open("this file probably does not exist")
-		if err != pack.ErrFileNotFound {
-			t.Fatal("expected open to return error")
-		}
-
-		if file != nil {
-			t.Fatal("expected file to be nil")
-		}
-	})
-
-	t.Run("list files", func(t *testing.T) {
-		names, err := store.List()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		var expected []string
-		for _, f := range files {
-			if path.Ext(f.key) == ".goback" {
-
-				name := strings.TrimSuffix(path.Base(f.key), path.Ext(f.key))
-				expected = append(expected, name)
-			}
-		}
-
-		if diff := cmp.Diff(expected, names); diff != "" {
-			t.Error("unexpected list of files")
-			t.Fatal(diff)
-		}
-	})
+	return files
 }
